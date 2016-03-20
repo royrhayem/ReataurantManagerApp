52e8fdc73b10eee5d88b9c6389caad21559bece0
royrhayem <royrhayem@gmail.com>
2016-03-20 16:58:00 +0200

    Initial Commit

---
+++ ReataurantManagerApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReataurantManagerApp
+//
+//  Created by Roy Rhayem on 3/20/16.
+//  Copyright © 2016 Roy Rhayem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReataurantManagerApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReataurantManagerApp
+//
+//  Created by Roy Rhayem on 3/20/16.
+//  Copyright © 2016 Roy Rhayem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
